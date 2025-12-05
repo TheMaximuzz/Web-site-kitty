a112aebe5b70ace615fda7e320cb15791f7cacfd
kakzheyapyanmariya <kakzheyapyanmariya@gmail.com>
2025-12-05 21:42:22 +0500

    Кастомные готовые котики
    Теперь картинки в разделе готовых котиков рисованные 2дшные, а не 3д реальные фотки.

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 # Готовые котики для главной страницы
 CATS = [
-    {"id": 1, "name": "Рыжик",  "price": 62300, "accessories_count": 2, "image": "cat1.jpg"},
-    {"id": 2, "name": "Багира", "price": 52000, "accessories_count": 2, "image": "cat2.jpg"},
-    {"id": 3, "name": "Снежок", "price": 57800, "accessories_count": 3, "image": "cat3.jpg"},
-    {"id": 4, "name": "Симка",  "price": 45500, "accessories_count": 1, "image": "cat4.jpg"},
-    {"id": 5, "name": "Дымок",  "price": 50800, "accessories_count": 2, "image": "cat5.jpg"},
-    {"id": 6, "name": "Персик", "price": 53800, "accessories_count": 3, "image": "cat6.jpg"}
+    {"id": 1, "name": "Киберкот",  "price": 62300, "accessories_count": 2, "image": "cat1.png"},
+    {"id": 2, "name": "Казикот", "price": 52000, "accessories_count": 2, "image": "cat2.png"},
+    {"id": 3, "name": "Мурослав", "price": 57800, "accessories_count": 3, "image": "cat3.png"},
+    {"id": 4, "name": "Цезамур",  "price": 45500, "accessories_count": 1, "image": "cat4.png"},
+    {"id": 5, "name": "Качкот",  "price": 50800, "accessories_count": 2, "image": "cat5.png"},
+    {"id": 6, "name": "Мурчёный", "price": 53800, "accessories_count": 3, "image": "cat6.png"}
 ]
 
 
